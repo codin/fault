cccf2138a0859045e56d2300869d32e641200b03
Kieron <kieron@fastmail.fm>
2021-04-05 20:29:43 +0100

    clean up

--- src/Handlers/JsonDump.php
+++ src/Handlers/JsonDump.php
@@ -5,46 +5,20 @@ declare(strict_types=1);
 namespace Codin\Fault\Handlers;
 
 use Codin\Fault\Contracts\ExceptionHandler;
-use Codin\Fault\Inspection;
-use Codin\Fault\Traits;
 use Throwable;
 
 class JsonDump implements ExceptionHandler
 {
-    use Traits\ExceptionMessage, Traits\ExceptionStack;
-
-    protected function getData(Throwable $e): array
-    {
-        $id = \sha1($e->getMessage());
-
-        $source = [];
-        foreach ($this->getStack($e) as $trace) {
-            $source[] = [
-                'exception' => $trace->getExceptionClassName().': '.$trace->getException()->getMessage(),
-                'trace' => \array_map(function (Inspection\Frame $frame) {
-                    return $frame->getFile().':'.$frame->getLine();
-                }, $trace->getFrames()),
-            ];
-        }
-
-        return [
-            'id' => $id,
-            'links' => [
-                'self' => $_SERVER['REQUEST_URI'] ?? '/',
-            ],
-            'status' => 500,
-            'code' => $e->getCode(),
-            'title' => \sprintf('Uncaught %s', \get_class($e)),
-            'detail' => $e->getMessage(),
-            'source' => $source,
-        ];
-    }
-
     public function handle(Throwable $e): void
     {
         if (!\headers_sent()) {
             \header('Content-Type: application/json', true, 500);
         }
-        echo \json_encode($this->getData($e), JSON_PRETTY_PRINT);
+        echo \json_encode([
+            'code' => $e->getCode(),
+            'title' => \get_class($e),
+            'detail' => $e->getMessage(),
+            'source' => (string) $e,
+        ], JSON_PRETTY_PRINT);
     }
 }
